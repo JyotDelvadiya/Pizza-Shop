9a6d8d5d1e92b0cd9872b96807dbdb04743d7324
JyotDelvadiya <tatva.pct205@outlook.com>
2025-01-29 19:00:05 +0530

    Dashboard sidebar navbar, user list, add user, edit user, delete user, roles page created

---
+++ assets/dist/js/dashboard.js
@@ -0,0 +1,46 @@
+const navLinks = document.querySelectorAll('.c-side');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function (event) {
+        navLinks.forEach(navLink => {
+            navLink.classList.remove('active');
+
+            const normalIcon = navLink.querySelector('.normal-icon');
+            const activeIcon = navLink.querySelector('.active-icon');
+
+            if (normalIcon && activeIcon) {
+                normalIcon.style.display = 'inline-block';
+                activeIcon.style.display = 'none';
+            }
+        });
+
+        this.classList.add('active');
+
+        const normalIcon = this.querySelector('.normal-icon');
+        const activeIcon = this.querySelector('.active-icon');
+
+        if (normalIcon && activeIcon) {
+            normalIcon.style.display = 'none';
+            activeIcon.style.display = 'inline-block';
+        }
+    });
+});
+
+function toggleMenu() {
+    const sidebar = document.querySelector('.sidebar');
+    const offcanvas = document.querySelector('.offcanvas');
+    
+    if (window.innerWidth < 769) {
+        sidebar.classList.toggle('d-block');
+        offcanvas.classList.toggle('show');
+        // sidebar.style.display = sidebar.style.display === 'none' ? 'block' : 'none';
+    }
+}
+
+function toggleMenuForBigScreen(){
+    const offcanvas = document.querySelector('.offcanvas');
+
+    if (window.innerWidth > 768) {
+        offcanvas.classList.toggle('show');
+    }
+}
